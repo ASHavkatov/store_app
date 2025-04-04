fb77ae09b0afd0c7800e7581768c94c3a29ebff9
MANIFESTOR-28032 <angrydragon983@gmail.com>
2025-04-04 19:22:16 +0500

    shunchaki

--- lib/onboarding_screen_splash/screen_splash/screen_splash_view.dart
+++ lib/onboarding_screen_splash/screen_splash/screen_splash_view.dart
@@ -1,10 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:store_app/core/routing/routes.dart';
 
-class ScreenSplashView extends StatelessWidget {
-  const ScreenSplashView({super.key});
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToMainScreen();
+  }
+
+  _navigateToMainScreen() {
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Routes.signUp),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            Image.asset(
+              'assets/images/Splash.jpg',
+              height: double.infinity,
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 20),
+            Text(
+              "My App",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
