4e3e50ba8703cdb90e9124661614a41c4853cf06
ASHavkatov <shavkatovasilbek086@gmail.com>
2025-04-06 18:22:58 +0500

    some chaneges

--- lib/features/authentication/login/pages/login_view.dart
+++ lib/features/authentication/login/pages/login_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:store_app/core/utils/colors.dart';
+import 'package:store_app/features/authentication/login/manager/login_view_model.dart';
 import '../../../../core/routing/routes.dart';
 import '../../../comman/presentations/store_app_form_field.dart';
 import '../../../comman/presentations/store_app_text.dart';
@@ -30,172 +31,176 @@ class _LoginViewState extends State<LoginView> {
   final TextEditingController emailController = TextEditingController();
 
   final TextEditingController passwordController = TextEditingController();
+  late final LoginViewModel vm;
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Scaffold(
+    return ListenableBuilder(
+      listenable: vm,
+      builder:(context, child)=> Scaffold(
         backgroundColor: Colors.white,
-        body: Padding(
-          padding: EdgeInsets.only(left: 25.w, right: 25.w, top: 59.h),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              StoreAppText(
-                title: "Login to your account",
-                color: Colors.black,
-                size: 32,
-                fontWeight: FontWeight.w600,
-              ),
-              SizedBox(height: 8.h),
-              StoreAppText(
-                title: "It’s great to see you again.",
-                color: Colors.black.withValues(alpha: 0.5),
-                size: 16,
-                fontWeight: FontWeight.w400,
-              ),
-              SizedBox(height: 25.h),
-              StoreAppFormField(
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Please enter some text";
-                  } else if (!isValid(value)) {
-                    return "Email doesn't match!";
-                  }
-                  return null;
-                },
-                onChanged: (value) {
-                  setState(() {
-                    isTrue = isValid(value);
-                  });
-                },
-                suffix: () {
-                  if (isTrue == null) {
-                    return const SizedBox();
-                  } else if (isTrue == true) {
-                    return SvgPicture.asset("assets/icons/store_app_true.svg");
-                  } else {
-                    return SvgPicture.asset("assets/icons/store_app_false.svg");
-                  }
-                }(),
-                title: 'Email',
-                hintText: "Enter your email address",
-                fontWeight: FontWeight.w500,
-                color: Colors.black,
-                controller: emailController,
-                size: 16,
-              ),
+        body: Form(
+          key: _formKey,
+          child: Padding(
+            padding: EdgeInsets.only(left: 25.w, right: 25.w, top: 59.h),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                StoreAppText(
+                  title: "Login to your account",
+                  color: Colors.black,
+                  size: 32,
+                  fontWeight: FontWeight.w600,
+                ),
+                SizedBox(height: 8.h),
+                StoreAppText(
+                  title: "It’s great to see you again.",
+                  color: Colors.black.withValues(alpha: 0.5),
+                  size: 16,
+                  fontWeight: FontWeight.w400,
+                ),
+                SizedBox(height: 25.h),
+                StoreAppFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter some text";
+                    } else if (!isValid(value)) {
+                      return "Email doesn't match!";
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    setState(() {
+                      isTrue = isValid(value);
+                    });
+                  },
+                  suffix: () {
+                    if (isTrue == null) {
+                      return const SizedBox();
+                    } else if (isTrue == true) {
+                      return SvgPicture.asset("assets/icons/store_app_true.svg");
+                    } else {
+                      return SvgPicture.asset("assets/icons/store_app_false.svg");
+                    }
+                  }(),
+                  title: 'Email',
+                  hintText: "Enter your email address",
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                  controller: emailController,
+                  size: 16,
+                ),
 
-              SizedBox(height: 16.h),
-              StoreAppFormField(
-                title: "Password",
-                hintText: "Enter your password",
-                fontWeight: FontWeight.w500,
-                color: Colors.black,
-                controller: passwordController,
-                size: 16,
-                suffix: SvgPicture.asset('assets/icons/store_app_hide.svg'),
-              ),
-              SizedBox(height: 10.h),
-              Row(
-                children: [
-                  Text(
-                    'Forgot your password? ',
-                    style: TextStyle(
-                      color: Colors.black.withValues(alpha: 0.5),
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      context.push(Routes.resetPassword);
-                    },
-                    child: Text(
-                      "Reset your password",
+                SizedBox(height: 16.h),
+                StoreAppFormField(
+                  title: "Password",
+                  hintText: "Enter your password",
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                  controller: passwordController,
+                  size: 16,
+                  suffix: SvgPicture.asset('assets/icons/store_app_hide.svg'),
+                ),
+                SizedBox(height: 10.h),
+                Row(
+                  children: [
+                    Text(
+                      'Forgot your password? ',
                       style: TextStyle(
-                        color: Colors.black,
+                        color: Colors.black.withValues(alpha: 0.5),
+                        fontSize: 14,
                         fontWeight: FontWeight.w400,
-                        fontSize: 14,
                       ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 24.h),
-              StoreFloatingButton(
-                text: "Login",
-                arrow: false,
-                callback: () {
-                  if (_formKey.currentState!.validate()) {
-                    ScaffoldMessenger.of(
-                      context,
-                    ).showSnackBar(const SnackBar(content: Text("Malades")));
-                  } else {
-                    ScaffoldMessenger.of(
-                      context,
-                    ).showSnackBar(const SnackBar(content: Text("Utolmading")));
-                  }
-                },
-                color: AppColors.primary900,
-              ),
-
-              SizedBox(height: 34),
-              Row(
-                children: [
-                  const Expanded(child: Divider()),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text(
-                      'Or',
-                      style: TextStyle(
-                        color: AppColors.primary500,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ),
-                  const Expanded(child: Divider()),
-                ],
-              ),
-              SizedBox(height: 24.h),
-              StoreSignUpContainer(
-                color: Colors.white,
-                title: "Login with Google",
-                svg: "assets/icons/store_app_google.svg",
-                fontColor: Colors.black,
-              ),
-              SizedBox(height: 16.h),
-              StoreSignUpContainer(
-                color: AppColors.darkBlue,
-                title: "Login with Facebook",
-                svg: "assets/icons/store_app_facebook.svg",
-                fontColor: Colors.white,
-              ),
-              SizedBox(height: 164.h),
-              Center(
-                child: Text.rich(
-                  TextSpan(
-                    text: "Don’t have an account?  ",
-                    style: TextStyle(color: AppColors.primary500, fontSize: 16),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: 'Join',
+                    GestureDetector(
+                      onTap: () {
+                        context.push(Routes.resetPassword);
+                      },
+                      child: Text(
+                        "Reset your password",
                         style: TextStyle(
-                          color: AppColors.primary900,
-                          fontSize: 15,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 14,
                         ),
-                        recognizer:
-                            TapGestureRecognizer()
-                              ..onTap = () {
-                                context.go(Routes.signUp);
-                              },
                       ),
-                    ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 24.h),
+                StoreFloatingButton(
+                  text: "Login",
+                  arrow: false,
+                  callback: () {
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(
+                        context,
+                      ).showSnackBar(const SnackBar(content: Text("Malades")));
+                    } else {
+                      ScaffoldMessenger.of(
+                        context,
+                      ).showSnackBar(const SnackBar(content: Text("Utolmading")));
+                    }
+                  },
+                  color: AppColors.primary900,
+                ),
+
+                SizedBox(height: 34),
+                Row(
+                  children: [
+                    const Expanded(child: Divider()),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                      child: Text(
+                        'Or',
+                        style: TextStyle(
+                          color: AppColors.primary500,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ),
+                    const Expanded(child: Divider()),
+                  ],
+                ),
+                SizedBox(height: 24.h),
+                StoreSignUpContainer(
+                  color: Colors.white,
+                  title: "Login with Google",
+                  svg: "assets/icons/store_app_google.svg",
+                  fontColor: Colors.black,
+                ),
+                SizedBox(height: 16.h),
+                StoreSignUpContainer(
+                  color: AppColors.darkBlue,
+                  title: "Login with Facebook",
+                  svg: "assets/icons/store_app_facebook.svg",
+                  fontColor: Colors.white,
+                ),
+                SizedBox(height: 164.h),
+                Center(
+                  child: Text.rich(
+                    TextSpan(
+                      text: "Don’t have an account?  ",
+                      style: TextStyle(color: AppColors.primary500, fontSize: 16),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Join',
+                          style: TextStyle(
+                            color: AppColors.primary900,
+                            fontSize: 15,
+                          ),
+                          recognizer:
+                              TapGestureRecognizer()
+                                ..onTap = () {
+                                  context.go(Routes.signUp);
+                                },
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
