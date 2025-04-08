55f01d96715358447cf9c95c11323e514d782e9b
Yukinari <icefrost816@gmail.com>
2025-04-08 13:09:56 +0500

    new widgets

--- lib/features/home/pages/home_view.dart
+++ lib/features/home/pages/home_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:store_app/features/authentication/reset_password/widgets/reset_password_app_bar.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:store_app/features/home/widgets/home_filter_box.dart';
+import 'package:store_app/features/home/widgets/search_container.dart';
+import 'package:store_app/features/home/widgets/store_icon_button_container.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -7,12 +10,48 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: ResetPasswordAppBar(),
-      body: ListView(
-        children: [
+      appBar: PreferredSize(
+        preferredSize: Size(double.infinity, 128.h),
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25.w),
+          child: AppBar(
+            leadingWidth: 150.w,
+            automaticallyImplyLeading: false,
+            leading: Text(
+              "Discover",
+              style: TextStyle(
+                fontSize: 32,
+                fontFamily: "GeneralSans",
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            actions: [
+              StoreIconButtonContainer(
+                image: "assets/icons/bell.svg",
+                callback: () {},
+                iconWidth: 24.w,
+                iconHeight: 24.h,
+              ),
+            ],
+            bottom: PreferredSize(
+              preferredSize: Size(double.infinity, 74),
+              child: Column(
+                children: [
+                  Row(
+                    spacing: 8.w,
+                    children: [
+                      SearchContainer(),
+                      HomeFilterBox(),
+                    ],
+                  ),
 
-        ],
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
+      body: ListView(children: []),
     );
   }
 }
