d016f20bc3ab764eeac7df1cad9c8d6d61e39c38
Fatxullo <fatxullofayzullayev6@gmail.com>
2025-04-09 11:34:53 +0500

    notification ui

--- lib/features/notification/pages/notification_view.dart
+++ lib/features/notification/pages/notification_view.dart
@@ -1,53 +1,116 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
 import 'package:store_app/core/utils/colors.dart';
 import 'package:store_app/features/common/presentations/store_app_app_bar.dart';
+import 'package:store_app/features/common/presentations/store_app_bottom_navigation_bar.dart';
+
+import '../../../core/routing/routes.dart';
 
 class NotificationView extends StatelessWidget {
-  const NotificationView({super.key});
+  NotificationView({super.key});
+
+  final item = [
+    NotificationItem(),
+    NotificationItem(),
+    NotificationItem(),
+    NotificationItem(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: StoreAppAppBar(title: "Notifications"),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 70),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SvgPicture.asset(
-                "assets/icons/store_app_full_big-ben.svg",
-                width: 64.w,
-                height: 64.h,
-                fit: BoxFit.cover,
-                colorFilter: ColorFilter.mode(AppColors.primary300, BlendMode.srcIn),
-              ),
-              SizedBox(height: 24.h),
-              Text(
-                "You haven’t gotten any notifications yet!",
-                textAlign: TextAlign.center,
-                maxLines: 2,
-                style: TextStyle(
-                  color: AppColors.primary900,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 20,
+      body:
+          item.length == 0
+              ? Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 70),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(
+                        "assets/icons/store_app_full_big-ben.svg",
+                        width: 64.w,
+                        height: 64.h,
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                          AppColors.primary300,
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                      SizedBox(height: 24.h),
+                      Text(
+                        "You haven’t gotten any notifications yet!",
+                        textAlign: TextAlign.center,
+                        maxLines: 2,
+                        style: TextStyle(
+                          color: AppColors.primary900,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                        ),
+                      ),
+                      SizedBox(height: 12.h),
+                      Text(
+                        "We’ll alert you when something cool happens.",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: AppColors.primary500,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(height: 12.h),
-              Text(
-                "We’ll alert you when something cool happens.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: AppColors.primary500,
-                  fontSize: 16,
-                ),
-              ),
-            ],
-          ),
-        ),
+              )
+              : NotificationItem(),
+      bottomNavigationBar: StoreAppBottomNavigationBar(
+        selectedIndex: 0,
+        value: (index) {
+          switch (index) {
+            case 0:
+              context.push(Routes.home);
+              break;
+            case 1:
+              context.push(Routes.home);
+              break;
+            case 2:
+              context.push(Routes.home);
+              break;
+            case 3:
+              context.push(Routes.home);
+              break;
+            case 4:
+              context.push(Routes.home);
+              break;
+          }
+        },
       ),
+    );
+  }
+}
+
+class NotificationItem extends StatelessWidget {
+  const NotificationItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(child: Text("salom")),
+        SizedBox(height: 20),
+        Center(child: Text("salom")),
+        SizedBox(height: 20),
+        Center(child: Text("salom")),
+        SizedBox(height: 20),
+        Center(child: Text("salom")),
+        SizedBox(height: 20),
+        Center(child: Text("salom")),
+        SizedBox(height: 20),
+        Center(child: Text("salom")),
+        SizedBox(height: 20),
+      ],
     );
   }
 }
