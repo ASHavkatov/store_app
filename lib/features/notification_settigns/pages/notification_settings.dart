1f1e764b35e6b677018144bd4fe8ee628f161f8c
ASHavkatov <shavkatovasilbek086@gmail.com>
2025-04-21 15:25:12 +0500

    some changes

--- lib/features/notification_settigns/pages/notification_settings.dart
+++ lib/features/notification_settigns/pages/notification_settings.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:store_app/core/utils/colors.dart';
 import 'package:store_app/features/common/presentations/store_app_app_bar.dart';
 import 'package:store_app/features/common/presentations/store_bottom_navigation_bar.dart';
+
+import '../widgets/settings_detail.dart';
 
 class NotificationSettingsView extends StatefulWidget {
   const NotificationSettingsView({super.key});
@@ -39,52 +40,3 @@ class _NotificationSettingsViewState extends State<NotificationSettingsView> {
   }
 }
 
-class SettingsDetail extends StatefulWidget {
-  final String title;
-
-  const SettingsDetail({super.key, required this.title});
-
-  @override
-  State<SettingsDetail> createState() => _SettingsDetailState();
-}
-
-class _SettingsDetailState extends State<SettingsDetail> {
-  bool status = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              widget.title,
-              style: TextStyle(
-                color: AppColors.primary900,
-                fontWeight: FontWeight.w500,
-                fontSize: 16.sp,
-              ),
-            ),
-            FlutteSwitch(
-              width: 45.w,
-              height: 21.h,
-              borderRadius: 40,
-              padding: 2.0,
-              activeColor: AppColors.primary900,
-              inactiveColor: AppColors.primary100,
-              value: status,
-              onToggle: (val) {
-                setState(() {
-                  status = val;
-                });
-              },
-            ),
-          ],
-        ),
-        SizedBox(height: 15.h),
-        Divider(height: 1, color: AppColors.primary200),
-      ],
-    );
-  }
-}
